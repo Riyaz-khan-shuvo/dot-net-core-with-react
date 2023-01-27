c96586562730bf72344a2dbfdf862da724ad8538
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2023-01-27 22:31:56 +0600

    department create has done

---
+++ src/Components/Department/CreateDepartment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateDepartment = () => {
+    return (
+        <div>
+            <h1>This is Create Department Page </h1>
+        </div>
+    );
+};
+
+export default CreateDepartment;
--- src/Components/Department/Department.js
+++ src/Components/Department/Department.js
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { departmentUrl } from '../../Data/Data';
@@ -9,13 +10,28 @@ const Department = () => {
         fetch(`${departmentUrl}`)
             .then(res => res.json())
             .then(data => setData(data))
-    }, [])
-    
+    }, [data])
+
+    const handleDelete = (e, id) => {
+        fetch(`${departmentUrl}/${id}`, {
+            method: "DELETE"
+        })
+            .then(res => res.text())
+            .then(result => {
+                if (result === "") {
+                    alert("Department deleted successful!!!")
+                }
+            })
+
+    }
 
     return (
         <div>
             <Layout>
                 <div className="container">
+                    <div className='my-3 text-end '>
+                        <Link to={"/department/create"} className="btn btn-primary">Add Department</Link>
+                    </div>
                     <table className="table table-hover table-striped">
                         <thead>
                             <tr>
@@ -26,15 +42,16 @@ const Department = () => {
                         </thead>
                         <tbody>
                             {
-                                data.map((data, index) => <tr>
+                                data.map((data, index) => <tr key={index}>
                                     <th scope="row">{index + 1} </th>
                                     <td> {data.departmentName} </td>
                                     <td>
                                         <Link to={`/department/edit/${data.id}`} className="text-success" >
                                             <i className="bi bi-pencil-square"></i>
                                         </Link>
-                                        <Link to={`/department/delete/${data.id}`} className="text-danger ms-3">
+                                        <Link onClick={(e) => handleDelete(e, data.id)} className="text-danger ms-3">
                                             <i className="bi bi-trash"></i>
+                                            {/* to={`/department/delete/${data.id}`} */}
                                         </Link>
                                     </td>
                                 </tr>)
