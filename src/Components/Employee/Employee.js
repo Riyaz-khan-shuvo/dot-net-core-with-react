64f6b7cb2932794ceb4718584f24bc8b4e35ef03
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2023-01-31 13:10:25 +0600

    employee show has done

--- src/Components/Employee/Employee.js
+++ src/Components/Employee/Employee.js
@@ -1,12 +1,57 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { departmentUrl } from '../../Data/Data';
 import Layout from '../Layout/Layout';
 
 const Employee = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        axios.get(`${departmentUrl}/GetEmployee`)
+            .then(data => setData(data.data))
+    }, [])
+    console.log(data)
+
     return (
         <div>
-            <Layout>
-                <h3>This is Employee Page </h3>
-            </Layout>
+            <div>
+                <Layout>
+                    <div className="container">
+                        <div className='my-3 text-end '>
+                            <Link to={"/Employee/create"} className="btn btn-primary">Add Department</Link>
+                        </div>
+                        <table className="table table-hover table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Employee Name </th>
+                                    <th scope="col">Department</th>
+                                    <th scope="col">Options </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    data.map((data, index) => <tr key={index}>
+                                        <td> {index + 1} </td>
+                                        <td> {data.name} </td>
+                                        <td> {data.department.departmentName} </td>
+                                        <td>
+                                            <Link className="text-success" >
+                                                <i className="bi bi-pencil-square"></i>
+                                            </Link>
+                                            <Link className="text-danger ms-3">
+                                                <i className="bi bi-trash"></i>
+                                                {/* to={`/department/delete/${data.id}`} */}
+                                            </Link>
+                                        </td>
+                                    </tr>)
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </Layout>
+            </div>
         </div>
     );
 };
